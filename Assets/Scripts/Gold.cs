a2d1792dd6be97f25750afb09d16f7bd950e8a10
Marc Zaku <mzaku@hotmail.de>
2020-10-20 11:53:03 +0200

    adds gold amount to the game

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Gold : MonoBehaviour {
+	public int goldAmount;
+	public Text goldAmountText;
+
+	void Update() {
+		this.goldAmountText.text = this.goldAmount.ToString("0 Gold");
+	}
+}
