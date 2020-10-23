31ba62c26c9b910c03b1f1552ee656a2f7ba51e3
Marc Zaku <mzaku@hotmail.de>
2020-10-23 14:08:37 +0200

    removes sample code

--- Assets/Scripts/DontDestroyOnLoad.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class DontDestroyOnLoad : MonoBehaviour {
-	void Awake() {
-		DontDestroyOnLoad(this.gameObject);
-	}
-}
--- Assets/Scripts/GameManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class GameManager : MonoBehaviour {
-	void Start() {
-		SceneManager.LoadScene("SampleScene");
-	}
-}
