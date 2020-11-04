c8302a063d6a550d53bcb1dc212826e62f2e66bf
Marc Zaku <mzaku@hotmail.de>
2020-11-04 15:58:26 +0100

    adds upgrade option to the game

--- Assets/Scripts/GoldProducer.cs
+++ Assets/Scripts/GoldProducer.cs
@@ -5,6 +5,7 @@ public class GoldProducer : MonoBehaviour {
 	public GoldProductionData GoldProductionData;
 	public Text goldAmountText;
 	public Text purchaseButtonLabel;
+	public Text upgradeButtonLabel;
 	public ProductionPopUp popupPrefab;
 	public Gold gold;
 	float elapsedTime;
@@ -13,16 +14,28 @@ public class GoldProducer : MonoBehaviour {
 		get => PlayerPrefs.GetInt(this.GoldProductionData.name, 0);
 		set {
 			PlayerPrefs.SetInt(this.GoldProductionData.name, value);
-			UpdateAmountLabel();
+			UpdateTitleLabel();
+		}
+	}
+
+	string UpgradeAmountPlayerPrefKey => $"{this.GoldProductionData.name}_Upgrade";
+
+	int UpgradeAmount {
+		get => PlayerPrefs.GetInt(this.UpgradeAmountPlayerPrefKey, 0);
+		set {
+			PlayerPrefs.SetInt(this.UpgradeAmountPlayerPrefKey, value);
+			UpdateTitleLabel();
 		}
 	}
 
 	bool IsAffordable => this.gold.GoldAmount >= this.GoldProductionData.GetActualCosts(this.Amount);
+	bool IsUpgradeAffordable => this.gold.GoldAmount >= this.GoldProductionData.GetActualCosts(this.UpgradeAmount);
 
 	public void SetUp(GoldProductionData goldProductionData) {
 		this.GoldProductionData = goldProductionData;
 		this.gameObject.name = goldProductionData.name;
 		this.purchaseButtonLabel.text = $"Purchase for {this.GoldProductionData.GetActualCosts(this.Amount)}";
+		this.upgradeButtonLabel.text = $"Upgrade for {this.GoldProductionData.GetActualCosts(this.UpgradeAmount)}";
 	}
 
 	public void Purchase() {
@@ -33,10 +46,18 @@ public class GoldProducer : MonoBehaviour {
 		}
 	}
 
-	void Start() {
-		UpdateAmountLabel();
+	public void Upgrade() {
+		if (this.IsUpgradeAffordable) {
+			this.gold.GoldAmount -= this.GoldProductionData.GetActualCosts(this.UpgradeAmount);
+			this.UpgradeAmount += 1;
+			this.upgradeButtonLabel.text = $"Upgrade for {this.GoldProductionData.GetActualCosts(this.UpgradeAmount)}";
+		}
 	}
-	
+
+	void Start() {
+		UpdateTitleLabel();
+	}
+
 	void Update() {
 		this.elapsedTime += Time.deltaTime;
 		if (this.elapsedTime >= this.GoldProductionData.productionTime) {
@@ -49,17 +70,22 @@ public class GoldProducer : MonoBehaviour {
 
 	void UpdateTextColor() {
 		this.purchaseButtonLabel.color = this.IsAffordable ? Color.black : Color.red;
+		this.upgradeButtonLabel.color = this.IsUpgradeAffordable ? Color.black : Color.red;
 	}
 
-	void UpdateAmountLabel() {
-		this.goldAmountText.text = this.Amount.ToString($"0 {this.GoldProductionData.name}");
+	void UpdateTitleLabel() {
+		this.goldAmountText.text = $"{this.Amount}x {this.GoldProductionData.name} Level {this.UpgradeAmount}";
 	}
 
 	void ProduceGold() {
 		if (this.Amount == 0)
 			return;
-		this.gold.GoldAmount += this.GoldProductionData.ProductionAmount * this.Amount;
+		this.gold.GoldAmount += Mathf.RoundToInt(CalculateProductionAmount());
 		var instance = Instantiate(this.popupPrefab, this.transform);
-		instance.GetComponent<Text>().text = $"+{this.GoldProductionData.ProductionAmount * this.Amount}";
+		instance.GetComponent<Text>().text = $"+{CalculateProductionAmount()}";
+	}
+
+	float CalculateProductionAmount() {
+		return this.GoldProductionData.GetProductionAmount(this.UpgradeAmount) * this.Amount;
 	}
 }
--- Assets/Scripts/GoldProductionData.cs
+++ Assets/Scripts/GoldProductionData.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 [CreateAssetMenu]
 public class GoldProductionData : ScriptableObject { 
-	// private to make sure that people not accidentally use 'costs' instead of 'actualCosts'
 	[SerializeField] int costs = 100;
-	// this variable is not needed by any other class anyways, hide anything that's not needed, keep the public API 'clean'
 	[SerializeField] float costMultiplier = 1.1f;
-	// (EXCLUSIVE ARGUMENT) least amount of code / easier to use
 	public float productionTime = 1f;
-	// (EXCLUSIVE ARGUMENT) make sure that people do not set / write this value on a scriptable object
 	[SerializeField] int productionAmount = 1;
-	public int ProductionAmount => this.productionAmount;
-	public Item item;
+	[SerializeField] float productionMultiplier = 1.05f;
 	
 	public int GetActualCosts(int amount) {
 		var result = this.costs * Mathf.Pow(this.costMultiplier, amount);
 		return Mathf.RoundToInt(result);
 	}
+	
+	public int GetProductionAmount(int upgradeAmount) {
+		var result = this.productionAmount * Mathf.Pow(this.productionMultiplier, upgradeAmount);
+		return Mathf.RoundToInt(result);
+	}
 }
-
