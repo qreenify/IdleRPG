2397676293d321b54878c86a7884280d8eff0151
Marc Zaku <mzaku@hotmail.de>
2020-11-10 11:47:41 +0100

    layouts the resource ui prefabs

--- Assets/Scripts/Resources/ResourceUI.cs
+++ Assets/Scripts/Resources/ResourceUI.cs
@@ -4,14 +4,12 @@ using UnityEngine.UI;
 namespace Resources {
 	public class ResourceUI : MonoBehaviour {
 		public Text amountText;
+		public Text resourceNameText;
 		public Resource resource;
 
-		void UpdateAmountLabel() {
-			this.amountText.text = this.resource.Owned.ToString($"0 {this.resource.name}");
-		}
-
 		void Update() {
-			UpdateAmountLabel();
+			this.amountText.text = this.resource.Owned.ToString();
+			this.resourceNameText.text = this.resource.name;
 		}
 
 		public void SetUp(Resource resource) {
