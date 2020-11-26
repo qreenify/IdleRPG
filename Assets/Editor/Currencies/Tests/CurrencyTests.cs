3e64fc6489a45ccb88f06b6b94cbfa0f72f65ea9
Marc Zaku <mzaku@hotmail.de>
2020-11-26 11:21:18 +0100

    moves all currency logic to a base class

--- Assets/Editor/Currencies/Tests/CurrencyTests.cs
+++ Assets/Editor/Currencies/Tests/CurrencyTests.cs
@@ -20,38 +20,50 @@ namespace Currencies.Tests {
 		public class Multiply {
 			[Test]
 			public void Result() {
-				var five = new Dollar(5);
-				Assert.AreEqual(new Dollar(10), five.Times(2));
-				Assert.AreEqual(new Dollar(15), five.Times(3));
+				var five = Money.Dollar(5);
+				Assert.AreEqual(Money.Dollar(10), five.Times(2));
+				Assert.AreEqual(Money.Dollar(15), five.Times(3));
 			}
 		
 			[Test]
 			public void DoesNotAffectOriginalInstance() {
-				var five = new Dollar(5);
+				var five = Money.Dollar(5);
 				five.Times(2);
-				Assert.AreEqual(new Dollar(5), five);
+				Assert.AreEqual(Money.Dollar(5), five);
 			}
 		}
-		
 
 		[Test]
-		public void EqualsTests() {
-			Assert.AreEqual(new Dollar(1), new Dollar(1));
-			Assert.AreNotEqual(new Dollar(1), new Dollar(2));
-			Assert.AreNotEqual(new Dollar(1), 1);
-			Assert.False(new Dollar(1).Equals(null));
+		public void SameCurrencyAmountsAreEqual() {
+			Assert.AreEqual(Money.Dollar(1), Money.Dollar(1));
+		}
+		
+		[Test]
+		public void DifferentAmountsAreInEqual() {
+			Assert.AreNotEqual(Money.Dollar(1), Money.Dollar(2));
+		}
+		
+		[Test]
+		public void DifferentCurrenciesAreUnEqual() {
+			Assert.AreNotEqual(Money.Dollar(1), Money.SEK(1));
+		}
+		
+		[Test]
+		public void OtherTypesAreUnEqual() {
+			Assert.AreNotEqual(Money.Dollar(1), 1);
+			Assert.False(Money.Dollar(1).Equals(null));
 		}
 		
 		[Test]
 		public void ToStringFormat() {
-			Assert.AreEqual("5 Dollar", new Dollar(5).ToString());
+			Assert.AreEqual("5 Dollar", Money.Dollar(5).ToString());
 		}
 
 		[Test]
 		public void MultiplySEK() {
-			var five = new SEK(5);
-			Assert.AreEqual(new SEK(10), five.Times(2)); 
-			Assert.AreEqual(new SEK(15), five.Times(3));
+			var five = Money.SEK(5);
+			Assert.AreEqual(Money.SEK(10), five.Times(2)); 
+			Assert.AreEqual(Money.SEK(15), five.Times(3));
 		}
 	}
 }
--- Assets/Scripts/Currencies/Money.cs
+++ Assets/Scripts/Currencies/Money.cs
@@ -1,9 +1,15 @@
 namespace Currencies {
 	public class Money {
 		protected int amount;
+		protected string currency;
 
+		public Money(int amount, string currency) {
+			this.amount = amount;
+			this.currency = currency;
+		}
+		
 		public override string ToString() {
-			return $"{this.amount} {GetType().Name}";
+			return $"{this.amount} {this.currency}";
 		}
 
 		public override bool Equals(object obj) {
@@ -11,7 +17,21 @@ namespace Currencies {
 				return false;
 			}
 			var money = (Money) obj;
+			if (this.currency != money.currency)
+				return false;
 			return money.amount == this.amount;
+		}
+
+		public static Money Dollar(int amount) {
+			return new Money(amount, "Dollar");
+		}
+
+		public static Money SEK(int amount) {
+			return new Money(amount, "SEK");
+		}
+		
+		public Money Times(int factor) {
+			return new Money(this.amount * factor, this.currency);
 		}
 	}
 }
