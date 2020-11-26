ed0d48f486d82f36917f894e73235fccadc20055
Marc Zaku <mzaku@hotmail.de>
2020-11-26 12:03:50 +0100

    currency sum test preparation

--- Assets/Editor/Currencies/Tests/CurrencyTests.cs
+++ Assets/Editor/Currencies/Tests/CurrencyTests.cs
@@ -31,6 +31,45 @@ namespace Currencies.Tests {
 			Assert.AreEqual(Money.Dollar(5), five);
 		}
 	}
+
+	public class Add {
+		/*
+		// Exercise 1:
+		[Test]
+		public void ResultForSameCurrencies() {
+			var five = Money.Dollar(5);
+			Assert.AreEqual(Money.Dollar(10), five.Add(five));
+		}
+
+		// Exercise 2:
+		[Test]
+		public void ResultForDifferentCurrencies() {
+			var exchangeRate = 10f;
+			var bank = new Bank(exchangeRate);
+			var fiveDollars = Money.Dollar(5);
+			var hundredSek = Money.SEK(100);
+			var sum = fiveDollars.Add(hundredSek); // Wallet || MoneyExpression (it is 5$ & 100 SEK)
+			var dollars = bank.ExchangeToDollar(sum); // Bank takes Wallet || MoneyExpression
+			Assert.AreEqual(Money.Dollar(15), dollars);
+		}
+		
+		// Exercise 3:
+		[Test]
+		public void ResultForMultipleCurrencies() {
+			var exchangeRates = ??; // euro -> usd 2:1 sek -> usd 10:1
+			var bank = new Bank(exchangeRates);
+			var fiveDollars = Money.Dollar(5);
+			var hundredSek = Money.SEK(100);
+			var twentyEuros = Money.Euro(10);
+			var sum = fiveDollars.Add(hundredSek); // Wallet || MoneyExpression (it is 5$ & 100 SEK)
+			sum = sum.Add(twentyEuros);
+			var dollars = bank.ExchangeToDollar(sum); // Bank takes Wallet || MoneyExpression
+			Assert.AreEqual(Money.Dollar(25), dollars);
+		}*/
+		
+		// maybe you want to change new Bank(exchangeRates) to:
+		// new Bank(); bank.AddExchangeRate(from: "Dollar", to: "Euro", rate: 2.0);
+	}
 	
 	// case 1: multiple classes in one namespace
 	namespace Equal {
