35e69fc060076c30899918110e4a507e64abccb9
Marc Zaku <mzaku@hotmail.de>
2020-11-26 11:43:58 +0100

    restructuring tests

--- Assets/Editor/Currencies/Tests/CurrencyTests.cs
+++ Assets/Editor/Currencies/Tests/CurrencyTests.cs
@@ -1,46 +1,40 @@
 ﻿using NUnit.Framework;
 
 namespace Currencies.Tests {
-
-	public class CSharpTests {
+	// case 1: only one class in a namespace => make the namespace to a class
+	/*
+	 * before:
+	 * namespace Multiply {
+	 *    class Tests {
+	 *    }
+	 * }
+	 */
+	public class Multiply {
 		[Test]
-		public void NewInstancesAreNotTheSame() {
-			Assert.AreNotSame(new object(), new object());
+		public void Result() {
+			var five = Money.Dollar(5);
+			Assert.AreEqual(Money.Dollar(10), five.Times(2));
+			Assert.AreEqual(Money.Dollar(15), five.Times(3));
 		}
-	}
 
-	public class RandomTests {
 		[Test]
-		public void RangeMaxIsNotInclusive() {
-			// write a test here that for example rolls 100 times (1-6) and tests, if 6 was included or not. 
+		public void ResultWithSEK() {
+			var five = Money.SEK(5);
+			Assert.AreEqual(Money.SEK(10), five.Times(2)); 
+			Assert.AreEqual(Money.SEK(15), five.Times(3));
+		}
+	
+		[Test]
+		public void DoesNotAffectOriginalInstance() {
+			var five = Money.Dollar(5);
+			five.Times(2);
+			Assert.AreEqual(Money.Dollar(5), five);
 		}
 	}
 	
-	public class CurrencyTests {
-		public class Multiply {
-			[Test]
-			public void Result() {
-				var five = Money.Dollar(5);
-				Assert.AreEqual(Money.Dollar(10), five.Times(2));
-				Assert.AreEqual(Money.Dollar(15), five.Times(3));
-			}
-
-			[Test]
-			public void ResultWithSEK() {
-				var five = Money.SEK(5);
-				Assert.AreEqual(Money.SEK(10), five.Times(2)); 
-				Assert.AreEqual(Money.SEK(15), five.Times(3));
-			}
-		
-			[Test]
-			public void DoesNotAffectOriginalInstance() {
-				var five = Money.Dollar(5);
-				five.Times(2);
-				Assert.AreEqual(Money.Dollar(5), five);
-			}
-		}
-
-		public class Equal {
+	// case 1: multiple classes in one namespace
+	namespace Equal {
+		public class SameType {
 			[Test]
 			public void SameCurrencyAmountsAreEqual() {
 				Assert.AreEqual(Money.Dollar(1), Money.Dollar(1));
@@ -55,14 +49,16 @@ namespace Currencies.Tests {
 			public void DifferentCurrenciesAreUnEqual() {
 				Assert.AreNotEqual(Money.Dollar(1), Money.SEK(1));
 			}
-		
+		}
+		public class DifferentType {
 			[Test]
 			public void OtherTypesAreUnEqual() {
 				Assert.AreNotEqual(Money.Dollar(1), 1);
 				Assert.False(Money.Dollar(1).Equals(null));
 			}
 		}
-		
+	}
+	public class General {
 		[Test]
 		public void ToStringFormat() {
 			Assert.AreEqual("5 Dollar", Money.Dollar(5).ToString());
